97bf8fd6f97cbd01571af70d244ca3d42edda1eb
xmatakt <timotej.matak@gmail.com>
2017-03-08 22:08:59 +0100

    prva xamarin app f zivote

---
+++ firstXamarinApp/Quotations/Quotations/Quotations/App.cs
@@ -0,0 +1,34 @@
+﻿using CoreGraphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Quotations
+{
+    public class App : Application
+    {
+        private List<string> quotations;
+        public App()
+        {
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ firstXamarinApp/Quotations/Quotations/Quotations/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Quotations
+{
+    public partial class MainPage : ContentPage
+    {
+        private double stepValue;
+        private List<string> quotations;
+        private int actualIndex = 0;
+
+        public MainPage()
+        {
+            stepValue = 1.0;
+            quotations = new List<string>()
+            {
+                "Je to tak, jak to je. L.J.",
+                "Dobry kod nikdy nie je zly.",
+                "Zly kod nikdy nie je dobry.",
+                "Hody boli vkoctene."
+            };
+
+            InitializeComponent();
+            Padding = Device.OnPlatform(
+                iOS: new Thickness(20, 30, 20, 20),
+                Android: new Thickness(20, 30, 20, 20),
+                WinPhone: new Thickness(20, 40, 20, 20)
+                );
+            quotationText.Text = quotations[actualIndex];
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var newStep = Math.Round(e.NewValue / stepValue);
+            fontSizeSlider.Value = newStep * stepValue;
+        }
+
+        void OnLeftButtonClick(object sender, EventArgs e)
+        {
+            if ((actualIndex - 1) >= 0)
+                quotationText.Text = quotations[--actualIndex];
+        }
+
+        void OnRightButtonClick(object sender, EventArgs e)
+        {
+            if ((actualIndex + 1) < quotations.Count)
+                quotationText.Text = quotations[++actualIndex];
+        }
+    }
+}
