acda53c16aade3194a5cdc3d45787c5f80fd7383
xmatakt <timotej.matak@gmail.com>
2017-04-12 07:24:11 +0200

    hotovo

--- MicrosoftCognitiveServices1/bonusCv8/bonusCv8/Form1.Designer.cs
+++ MicrosoftCognitiveServices1/bonusCv8/bonusCv8/Form1.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBoxLeft = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRight = new System.Windows.Forms.PictureBox();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.labelLeft = new System.Windows.Forms.Label();
+            this.labelRight = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBoxLeft
+            // 
+            this.pictureBoxLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxLeft.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxLeft.Name = "pictureBoxLeft";
+            this.pictureBoxLeft.Size = new System.Drawing.Size(277, 311);
+            this.pictureBoxLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLeft.TabIndex = 0;
+            this.pictureBoxLeft.TabStop = false;
+            // 
+            // pictureBoxRight
+            // 
+            this.pictureBoxRight.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxRight.Location = new System.Drawing.Point(295, 12);
+            this.pictureBoxRight.Name = "pictureBoxRight";
+            this.pictureBoxRight.Size = new System.Drawing.Size(277, 311);
+            this.pictureBoxRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRight.TabIndex = 1;
+            this.pictureBoxRight.TabStop = false;
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(12, 329);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(127, 35);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.Text = "Choose image...";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(295, 329);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(127, 35);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.Text = "Choose image...";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // labelLeft
+            // 
+            this.labelLeft.AutoSize = true;
+            this.labelLeft.Location = new System.Drawing.Point(12, 393);
+            this.labelLeft.Name = "labelLeft";
+            this.labelLeft.Size = new System.Drawing.Size(79, 17);
+            this.labelLeft.TabIndex = 4;
+            this.labelLeft.Text = "Happiness:";
+            this.labelLeft.TextChanged += new System.EventHandler(this.labelLeft_TextChanged);
+            // 
+            // labelRight
+            // 
+            this.labelRight.AutoSize = true;
+            this.labelRight.Location = new System.Drawing.Point(292, 393);
+            this.labelRight.Name = "labelRight";
+            this.labelRight.Size = new System.Drawing.Size(79, 17);
+            this.labelRight.TabIndex = 5;
+            this.labelRight.Text = "Happiness:";
+            this.labelRight.TextChanged += new System.EventHandler(this.labelRight_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 489);
+            this.ClientSize = new System.Drawing.Size(585, 443);
+            this.Controls.Add(this.labelRight);
+            this.Controls.Add(this.labelLeft);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.pictureBoxRight);
+            this.Controls.Add(this.pictureBoxLeft);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLeft;
+        private System.Windows.Forms.PictureBox pictureBoxRight;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label labelLeft;
+        private System.Windows.Forms.Label labelRight;
     }
 }
 
--- MicrosoftCognitiveServices1/bonusCv8/bonusCv8/Form1.cs
+++ MicrosoftCognitiveServices1/bonusCv8/bonusCv8/Form1.cs
@@ -1,20 +1,133 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace bonusCv8
 {
     public partial class Form1 : Form
     {
+        private bool isLeftLabelFilled = false;
+        private bool isRightLabelFilled = false;
+        private int timeToSleep = 100;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = openFileDialog.FileName;
+                pictureBoxLeft.Image = new Bitmap(fileName);
+                pictureBoxLeft.Update();
+                pictureBoxLeft.Refresh();
+                MakeRequest(fileName, labelLeft);
+            }
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = openFileDialog.FileName;
+                pictureBoxRight.Image = new Bitmap(fileName);
+                pictureBoxRight.Update();
+                pictureBoxRight.Refresh();
+                MakeRequest(fileName, labelRight);
+            }
+        }
+
+        private byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        private async void MakeRequest(string imageFilePath, Label label)
+        {
+            var client = new HttpClient();
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "e00b84f9752b4f409fb62216616fa6c0");
+
+            string uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?";
+            HttpResponseMessage response;
+            string responseContent;
+
+            // Request body. Try this sample with a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                // This example uses content type "application/octet-stream".
+                // The other content types you can use are "application/json" and "multipart/form-data".
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+                responseContent = response.Content.ReadAsStringAsync().Result;
+            }
+
+            //A peak at the JSON response.
+            label.Text = "Happiness: " + responseContent.Split(',').FirstOrDefault(x => x.Contains("happiness")).Split(':')[1];
+            //richTextBox.Text = responseContent;
+        }
+
+        private void labelLeft_TextChanged(object sender, EventArgs e)
+        {
+            isLeftLabelFilled = true;
+            GetWinner();
+        }
+
+        private void labelRight_TextChanged(object sender, EventArgs e)
+        {
+            isRightLabelFilled = true;
+            GetWinner();
+        }
+
+        private void GetWinner()
+        {
+            if(isLeftLabelFilled && isRightLabelFilled)
+            {
+                var leftHappiness = float.Parse(labelLeft.Text.Split(' ')[1]);
+                var rightHappiness = float.Parse(labelRight.Text.Split(' ')[1]);
+
+                if(leftHappiness > rightHappiness)
+                {
+                    var originalImage = pictureBoxLeft.Image;
+                    for (int i = 0; i < 5; i++)
+                    {
+                        pictureBoxLeft.Image = Properties.Resources.winner;
+                        pictureBoxLeft.Refresh();
+                        System.Threading.Thread.Sleep(timeToSleep);
+                        pictureBoxLeft.Image = originalImage;
+                        pictureBoxLeft.Refresh();
+                        System.Threading.Thread.Sleep(timeToSleep);
+                    }
+                    return;
+                }
+
+                if (leftHappiness < rightHappiness)
+                {
+                    var originalImage = pictureBoxRight.Image;
+                    for (int i = 0; i < 5; i++)
+                    {
+                        pictureBoxRight.Image = Properties.Resources.winner;
+                        pictureBoxRight.Refresh();
+                        System.Threading.Thread.Sleep(timeToSleep);
+                        pictureBoxRight.Image = originalImage;
+                        pictureBoxRight.Refresh();
+                        System.Threading.Thread.Sleep(timeToSleep);
+                    }
+                    return;
+                }
+
+                MessageBox.Show("Obe osoby su rovnako stastne!");              
+            }
         }
     }
 }
